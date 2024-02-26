f32e190dc062f02bd208574239ef869209ecf61a
Kajetan Olbryt <k.olbryt@gmail.com>
2024-02-26 16:19:38 +0100

    Add header and date validation. Start hour validation. Change csv header

--- index.php
+++ index.php
@@ -9,6 +9,47 @@
         }     
         return $sleep_data;
     }
+
+    function is_valid_date($date_string) {
+        $format = 'Y.m.d';
+        $date = DateTime::createFromFormat($format, $date_string);
+        return $date && $date->format($format) === $date_string;
+    }
+
+    function is_validate_hours($from_1, $to_1, $from_2, $to_2, $from_3, $to_3) {
+        $format = 'H:i';
+        $time = strtotime($time_str);
+        if ($time !== false && date($format, $time) == $time_str) {
+            
+        }
+    }
+
+    function is_valid_sleep_data($sleep_data) {
+        if(empty($sleep_data)) {
+            echo "Error! No data or it's empty";
+            return false;
+        }
+        if($sleep_data[0][0] != "date"   ||
+           $sleep_data[0][1] != "from_1" ||
+           $sleep_data[0][2] != "to_1"   ||
+           $sleep_data[0][3] != "from_2" ||
+           $sleep_data[0][4] != "to_1"   ||
+           $sleep_data[0][5] != "from_3" ||
+           $sleep_data[0][6] != "to_3"
+        ) {
+            echo 'Error! Wrong header format';
+            return false;
+        }
+        $row_count = count($sleep_data);
+        for($i = 1; $i <= $row_count; $i++) {
+            if(!is_valid_date($sleep_data[$i][0])) {
+                echo 'Error! Wrong date format at row '.$i;
+                return false;
+            }
+
+        }
+        return true;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,13 +58,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Badanie snu</title>
-
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="content">
         <?php
-            $stats = get_data_from_file();
+            $sleep_data = get_data_from_file();
+            echo is_valid_sleep_data($sleep_data);
         ?>
         <!-- <div class="day">
             <span class="day_number">2023.10.16</span>
